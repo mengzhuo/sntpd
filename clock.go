0cfc27352783d848768502f6e7abcfb1d59be07f
Meng Zhuo <mengzhuo1203@gmail.com>
2017-05-24 09:32:56 +0000

    add clockselect

--- clock.go
+++ clock.go
@@ -13,6 +13,13 @@ const (
 	AllanXpt    = time.Hour
 	MaxDistance = 1.5 // second
 	NStage      = 8
+	MaxStratum  = 16
+)
+
+const (
+	TypeLower = -1 + iota
+	TypeMid
+	TypeUpper
 )
 
 type Clock struct {
@@ -163,8 +170,109 @@ func float64Max(a, b float64) float64 {
 	return b
 }
 
+type Interset struct {
+	peer *Peer
+	val  float64
+	typE int
+}
+
+func (s *Service) fit(p *Peer) (err error) {
+	if p.leap == NotSync {
+		return PeerNotSync
+	}
+	if p.stratum >= MaxStratum {
+		return PeerInvalidStratum
+	}
+
+	if p.rootDistance > MaxDistance+Phi*log2D(s.clock.poll) {
+		return PeerRootDistanceTooBig
+	}
+	if time.Now().Before(p.epoch) {
+		return PeerRootDistanceTooBig
+	}
+	if p.reach&0x07 == 0 {
+		return PeerNotAvailable
+	}
+	return
+}
+
 func (s *Service) clockSelect() {
 
+	samples := []Interset{}
+	for _, p := range s.clock.peer {
+		if err := s.fit(p); err != nil {
+			log.Printf("clockSelect: %s not fit, reason:%s", p.addr, err)
+			continue
+		}
+		samples = append(samples, Interset{p, p.offset - p.rootDistance, TypeLower})
+		samples = append(samples, Interset{p, p.offset + p.rootDistance, TypeUpper})
+	}
+
+	if len(samples) < 2 {
+		log.Printf("clockSelect: not enough peer to select cluster surviors")
+		return
+	}
+	s.marzullo(samples)
+}
+
+func (s *Service) marzullo(iset []Interset) (surviors []*Peer) {
+
+	sort.Sort(byOffset(iset))
+	nlist := len(iset) / 2
+	nl2 := len(iset)
+
+	var n int
+	low := float64(3600)
+	high := float64(-3600)
+
+	for allow := 0; 2*allow < nlist; allow++ {
+
+		n = 0
+		for _, set := range iset {
+			low = set.val
+			n -= set.typE
+			if n >= nlist-allow {
+				break
+			}
+		}
+
+		n = 0
+		for j := nl2 - 1; j > 0; j-- {
+			high = iset[j].val
+			n += iset[j].typE
+			if n >= nlist-allow {
+				break
+			}
+		}
+		if high > low {
+			break
+		}
+	}
+
+	var p *Peer
+	for i := 0; i < len(iset); i += 2 {
+		p = iset[i].peer
+		if high <= low || p.offset+p.rootDistance < low || p.offset-p.rootDistance > high {
+			continue
+		}
+		surviors = append(surviors, p)
+	}
+	return
+
+}
+
+type byOffset []Interset
+
+func (b byOffset) Less(i, j int) bool {
+	return b[i].val < b[j].val
+}
+
+func (b byOffset) Len() int {
+	return len(b)
+}
+
+func (b byOffset) Swap(i, j int) {
+	b[i], b[j] = b[j], b[i]
 }
 
 func (s *Service) clockUpdate() {
